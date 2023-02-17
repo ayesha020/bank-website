b42c5f409cc6004e3267dfceee9b4236c0b76720
ayesha020 <ayesha50-020@diu.edu.bd>
2023-02-18 02:42:06 +0600

    update

--- js/deposit.js
+++ js/deposit.js
@@ -1,14 +1,8 @@
-// check-console.log('deposit.js');
-
-// step-1:add event listener to the deposit button
-document.getElementById('btn-deposit').addEventListener('click', function(){
-
-    // step-2: get the deposite amount from the deposite input field
+document.getElementById('btn-deposit').addEventListener('click',function(){
     const depositField = document.getElementById('deposit-field');
     const depositAmount = depositField.value;
-//    step-3:get the current deposit total 
-const depositTotalElement = document.getElementById('deposit-total');
-const depositTotal = depositTotalElement.innerNumber;
-depositTotalElement.innerNumber = depositAmount;
-
+   const depositTotalElement =document.getElementById('deposit-total');
+   const depositTotal =depositTotalElement.innerText;
+   depositTotalElement.innerText = depositAmount;
+   console.log(depositTotal);
 })
