6b20ff1f9785a4ecb7c22b4ae9a8a3a8002bea9f
ayesha020 <ayesha50-020@diu.edu.bd>
2023-02-18 15:34:45 +0600

    update deposit

--- js/deposit.js
+++ js/deposit.js
@@ -1,8 +1,50 @@
+// step-1:add event listner to the deposit button
+
 document.getElementById('btn-deposit').addEventListener('click',function(){
+
+    // s-2: get the deposite amount from the deposite input field 
+
     const depositField = document.getElementById('deposit-field');
-    const depositAmount = depositField.value;
+    const newDepositAmountString = depositField.value;
+
+    const newDepositAmount = parseFloat(newDepositAmountString);
+
+// s-3: get the current deposite total
+
    const depositTotalElement =document.getElementById('deposit-total');
-   const depositTotal =depositTotalElement.innerText;
-   depositTotalElement.innerText = depositAmount;
-   console.log(depositTotal);
+
+   const previousDepositTotalString = depositTotalElement.innerText;
+
+   const previousDepositTotal = parseFloat(previousDepositTotalString);
+
+//  s-4: add numbers to set the total deposit   
+
+console.log(typeof previousDepositTotal);
+
+const currentDepositTotal = previousDepositTotal+ newDepositAmount;
+
+   depositTotalElement.innerText = currentDepositTotal;
+
+//    console.log(depositTotal);
+// s-5:get balance current total
+
+const balanceTotalElement = document.getElementById('balance-total');
+const previousBalanceTotalString = balanceTotalElement.innerText; 
+const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+// s-6: calculate current balance
+
+const currentBalanceTotal =previousBalanceTotal + newDepositAmount;
+
+// set the balance total
+
+balanceTotalElement.innerText = currentBalanceTotal;
+
+
+
+// s-7:clear the deposit field
+
+depositField.value='';
+console.log(typeof newDepositAmount);
+
 })
